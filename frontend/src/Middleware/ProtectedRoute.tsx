48c3bf1c0a1f74ee334ada1c77397c56bb4fd347
MikeTryArtd <mikeleano26@gmail.com>
2026-04-18 20:05:22 +0800

    initial admin check for admin dashboard + will add later more

--- frontend/src/Middleware/ProtectedRoute.tsx
+++ frontend/src/Middleware/ProtectedRoute.tsx
@@ -2,11 +2,23 @@ import type { ReactNode } from "react";
 import { Navigate } from "react-router";
 
 const ProtectedRoute = ({ children }: {children: ReactNode}) => {
+ 
   const token = sessionStorage.getItem('token');
 
   if (!token) {
     return <Navigate to="/Signin" replace />;
   }
+
+
+  //I WILL ADD USER TYPE LATER ON FOR NOW ITS LIKE THIS CAUSE IM TOO FUCKING LAZY...//
+  const currentPath = window.location.pathname;
+  if (currentPath === "/Admin")
+  {
+    alert ("User not allowed");
+    return <Navigate to="/Home" replace />;
+  }
+
+  
 
   return children;
 };
